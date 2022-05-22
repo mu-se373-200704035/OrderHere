fe8fbe9261d3eb0d2beeb869b4654e1bb12bc456
bozgen <bilalozgen00@gmail.com>
2022-05-22 08:08:49 +0300

    root page style

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,42 @@
-import { IonButton, IonContent, IonPage,} from '@ionic/react';
+import "./Root.css";
+import { IonContent, IonPage,} from '@ionic/react';
+import { useHistory } from 'react-router';
+import { MainContext, useContext } from '../components/Context';
 const Root = () => {
+    
+    const history = useHistory();
+    const {setCurrentPageDetails} = useContext(MainContext); 
+    
+    const gotoShops =  ()=>{
+        setCurrentPageDetails((prev: any)=>{
+            return{
+                ...prev,
+                page: "/shops"
+            }
+        }); 
+        history.push("/shops") 
+    }
+    const gotoAdminOrders =  ()=>{
+        setCurrentPageDetails((prev: any)=>{
+            return{
+                ...prev,
+                page: "/admin/orders"
+            }
+        });
+        history.push("/admin/orders");
+    }
+
     return (
         <IonPage>
           <IonContent fullscreen>
-              <IonButton routerLink='/shops'>I am a customer</IonButton>
-              <IonButton routerLink='/admin/orders'>I am an employee</IonButton>
+            <section onClick={gotoShops} className="root customer">
+                <h1>I am a customer</h1>
+            </section>
+
+
+            <section onClick={gotoAdminOrders}className="root employee">
+                <h1>I am an employee</h1>
+            </section>
           </IonContent>
         </IonPage>
     )
